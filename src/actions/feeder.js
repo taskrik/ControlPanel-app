7845230173daf8074506d66afc843c2f9a499f9d
taskrik <tasos.krikonis@gmail.com>
2019-04-19 23:41:53 +0200

    Creates actions for opening and closing the feeder

---
+++ src/actions/feeder.js
@@ -0,0 +1,55 @@
+import Axios from "axios";
+import Config from "react-native-config";
+
+const feeder = {
+  OPEN_FEEDER: "feeder/OPEN_FEEDER",
+  CLOSE_FEEDER: "feeder/CLOSE_FEEDER",
+  FAILED_TO_OPEN_FEEDER: "feeder/FAILED_TO_OPEN_FEEDER",
+  FAILED_TO_CLOSE_FEEDER: "feeder/FAILED_TO_CLOSE_FEEDER"
+};
+
+export default feeder;
+
+export const openFeeder = () => dispatch => {
+  url = `${Config.API_URL}` + "/on";
+  console.log("url:", url);
+
+  Axios.post(url)
+    .then(res => res.data)
+    .then(data => {
+      dispatch({
+        type: feeder.OPEN_FEEDER,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: feeder.FAILED_TO_OPEN_FEEDER,
+        error: error.message
+      });
+    });
+};
+
+export const closeFeeder = () => dispatch => {
+  dispatch({
+    type: feeder.CLOSE_FEEDER
+  });
+
+  url = `${Config.API_URL}/off`;
+  console.log("url: ", url);
+
+  Axios.post(url)
+    .then(res => res.data)
+    .then(data => {
+      dispatch({
+        type: feeder.CLOSE_FEEDER,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: feeder.FAILED_TO_CLOSE_FEEDER,
+        error: error.message
+      });
+    });
+};
