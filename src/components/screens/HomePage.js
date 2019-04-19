8d5d0875eb874bf20af2138b002e20db723a09a9
taskrik <tasos.krikonis@gmail.com>
2019-04-19 23:40:43 +0200

    Creates the home page where all the tools going to be

--- src/components/screens/HomePage.js
+++ src/components/screens/HomePage.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { connect } from "react-redux";
+import { TouchableOpacity } from "react-native-gesture-handler";
+
 
 export class HomePage extends Component {
   render() {
     return (
-      <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> Hello this is the HomePage </Text>
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <TouchableOpacity
+          onPress={() => this.props.navigation.navigate('CatFeeder')}
+          style={{
+            borderRadius: 10,
+            width: 150,
+            height: 50,
+            alignItems: "center",
+            justifyContent: "center",
+            backgroundColor: "blue"
+          }}
+        >
+          <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
+            Cat Feeder
+          </Text>
+        </TouchableOpacity>
       </View>
     );
   }
