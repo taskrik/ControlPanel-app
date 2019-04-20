3727b29c7f96ffd7a42e0ea25dd06aba6ec24a17
taskrik <tasos.krikonis@gmail.com>
2019-04-21 01:21:10 +0200

    Displays the message from the server WIP

--- src/components/screens/CatFeeder.js
+++ src/components/screens/CatFeeder.js
@@ -1,45 +1,46 @@
 import React, { Component } from "react";
-import { View, Text, Button } from "react-native";
-import { connect } from "react-redux";
+import { Text, View, StyleSheet } from "react-native";
 import { TouchableOpacity } from "react-native-gesture-handler";
-import { openFeeder, closeFeeder } from "../../actions/feeder";
+import { connect } from "react-redux";
+
+import { closeFeeder, openFeeder } from "../../actions/feeder";
 
 export class CatFeeder extends Component {
   render() {
+    const { serverMessage } = this.props.feeder;
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        <View
+          style={{
+            marginBottom: 50,
+            borderWidth: 0.5,
+            height: 200,
+            width: 300,
+            alignItems: "center",
+            justifyContent: "center"
+          }}
+        >
+          {!serverMessage && (
+            <Text style={{ fontSize: 18 }}>Its feeding time!</Text>
+          )}
+          {serverMessage && (
+            <Text style={{ fontSize: 18 }}>{serverMessage.message}</Text>
+          )}
+        </View>
         <View>
           <TouchableOpacity
             onPress={() => this.props.openFeeder()}
-            style={{
-              width: 150,
-              height: 60,
-              marginBottom: 20,
-              backgroundColor: "green",
-              borderRadius: 10,
-              alignItems: "center",
-              justifyContent: "center"
-            }}
+            style={styles.button}
           >
             <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
-              Feed Luke
+              Open Feeder
             </Text>
           </TouchableOpacity>
           <TouchableOpacity
             onPress={() => this.props.closeFeeder()}
-            style={{
-              width: 150,
-              height: 60,
-              marginBottom: 20,
-              backgroundColor: "red",
-              borderRadius: 10,
-              alignItems: "center",
-              justifyContent: "center"
-            }}
+            style={{ ...styles.button, ...{ backgroundColor: "red" } }}
           >
-            <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
-              Stop feeder
-            </Text>
+            <Text style={styles.text}>Stop Feeder</Text>
           </TouchableOpacity>
         </View>
       </View>
@@ -47,12 +48,27 @@ export class CatFeeder extends Component {
   }
 }
 
-const mapStateToProps = state => ({});
+const mapStateToProps = state => ({
+  feeder: state.feeder
+});
 
 const mapDispatchToProps = {
   openFeeder,
   closeFeeder
 };
+
+const styles = StyleSheet.create({
+  button: {
+    width: 150,
+    height: 60,
+    marginBottom: 20,
+    borderRadius: 10,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "green"
+  },
+  text: { fontSize: 18, color: "white", fontWeight: "bold" }
+});
 
 export default connect(
   mapStateToProps,
