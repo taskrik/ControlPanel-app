e98eb51fe70757eb5341206e76ac6384fef7360c
taskrik <tasos.krikonis@gmail.com>
2019-04-20 23:36:13 +0200

    Installs react native vector icons

--- android/app/src/main/java/com/controlpanelapp/MainApplication.java
+++ android/app/src/main/java/com/controlpanelapp/MainApplication.java
@@ -3,6 +3,7 @@ package com.controlpanelapp;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.oblador.vectoricons.VectorIconsPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.lugg.ReactNativeConfig.ReactNativeConfigPackage;
 import com.facebook.react.ReactNativeHost;
@@ -25,6 +26,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new VectorIconsPackage(),
             new RNGestureHandlerPackage(),
             new ReactNativeConfigPackage()
       );
