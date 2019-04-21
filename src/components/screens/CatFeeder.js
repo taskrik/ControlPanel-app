ca092559f0ebb3525e9f2ecfd7ff37833b10bee2
taskrik <tasos.krikonis@gmail.com>
2019-04-21 14:21:14 +0200

    Replacing touchableOpacity with native base buttons and adds disable

--- src/components/screens/CatFeeder.js
+++ src/components/screens/CatFeeder.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
 import { Text, View, StyleSheet } from "react-native";
+import { Button } from "native-base";
 import { TouchableOpacity } from "react-native-gesture-handler";
 import { connect } from "react-redux";
 
 import { closeFeeder, openFeeder } from "../../actions/feeder";
 
 export class CatFeeder extends Component {
+  state = {
+    disableOpenButton: false,
+    disableCloseButton: true
+  };
+
   render() {
     const { serverMessage } = this.props.feeder;
     return (
@@ -25,28 +31,56 @@ export class CatFeeder extends Component {
           )}
           {serverMessage && (
             <View style={{ alignItems: "center", justifyContent: "center" }}>
-              <Text style={{ fontSize: 26, marginBottom:20 }}>
-                You fed the cat {serverMessage.timesUsed} times
+              <Text style={{ fontSize: 26, marginBottom: 20 }}>
+                You fed the cat {serverMessage.feederInfo.timesUsed} times
               </Text>
-              <Text style={{ fontSize: 20, color: serverMessage.message === 'Feeder closed!'? 'red' : 'green'  }}>{serverMessage.message}</Text>
+
+              <Text style={{ fontSize: 20, marginBottom: 20 }}>
+                Last: {serverMessage.feederInfo.hours[0]}
+              </Text>
+              <Text
+                style={{
+                  fontSize: 20,
+                  color:
+                    serverMessage.message === "Feeder closed!" ? "red" : "green"
+                }}
+              >
+                {serverMessage.message}
+              </Text>
             </View>
           )}
         </View>
         <View>
-          <TouchableOpacity
-            onPress={() => this.props.openFeeder()}
-            style={styles.button}
+          <Button
+            style={{...styles.button,
+              backgroundColor: this.state.disableOpenButton ? "grey" : "green"
+            }}
+            onPress={() => {
+              this.setState({
+                disableOpenButton: !this.state.disableOpenButton,
+                disableCloseButton: !this.state.disableCloseButton
+              });
+              this.props.openFeeder();
+            }}
+            disabled={this.state.disableOpenButton}
           >
-            <Text style={{ fontSize: 18, color: "white", fontWeight: "bold" }}>
-              Open Feeder
-            </Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => this.props.closeFeeder()}
-            style={{ ...styles.button, ...{ backgroundColor: "red" } }}
+            <Text style={styles.text}>Open feeder</Text>
+          </Button>
+          <Button
+            disabled={this.state.disableCloseButton}
+            style={{...styles.button,
+              backgroundColor: this.state.disableCloseButton ? "grey" : "red"
+            }}
+            onPress={() => {
+              this.setState({
+                disableOpenButton: !this.state.disableOpenButton,
+                disableCloseButton: !this.state.disableCloseButton
+              });
+              this.props.closeFeeder();
+            }}
           >
             <Text style={styles.text}>Stop Feeder</Text>
-          </TouchableOpacity>
+          </Button>
         </View>
       </View>
     );
