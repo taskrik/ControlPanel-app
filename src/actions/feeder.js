95ba4e05b69a020e8743dc29dd6a245973888553
taskrik <tasos.krikonis@gmail.com>
2019-04-22 13:48:45 +0200

    Create actions for reset counter and to get the current state

--- src/actions/feeder.js
+++ src/actions/feeder.js
@@ -5,10 +5,34 @@ const feeder = {
   OPEN_FEEDER: "feeder/OPEN_FEEDER",
   CLOSE_FEEDER: "feeder/CLOSE_FEEDER",
   FAILED_TO_OPEN_FEEDER: "feeder/FAILED_TO_OPEN_FEEDER",
-  FAILED_TO_CLOSE_FEEDER: "feeder/FAILED_TO_CLOSE_FEEDER"
+  FAILED_TO_CLOSE_FEEDER: "feeder/FAILED_TO_CLOSE_FEEDER",
+  RESET_FEEDER: "feeder/RESET_FEEDER",
+  FAILED_TO_RESET_FEEDERR: "feeder/FAILED_TO_RESET_FEEDER",
+  GET_FEEDER_STATE: "feeder/GET_FEEDER_STATE",
+  FAILED_TO_GET_FEEDER_STATE: "feeder/FAILED_TO_GET_FEEDER_STATE"
 };
 
 export default feeder;
+
+export const getFeederState = () => dispatch => {
+  url = `${Config.API_URL}`;
+
+  Axios.get(url)
+    .then(res => res.data)
+    .then(data => {
+      dispatch({
+        type: feeder.GET_FEEDER_STATE,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: feeder.FAILED_TO_GET_FEEDER_STATE,
+        error: error.message
+      });
+    });
+};
+
 
 export const openFeeder = () => dispatch => {
   url = `${Config.API_URL}` + "/on";
@@ -43,6 +67,25 @@ export const closeFeeder = () => dispatch => {
     .catch(error => {
       dispatch({
         type: feeder.FAILED_TO_CLOSE_FEEDER,
+        error: error.message
+      });
+    });
+};
+
+export const resetFeeder = () => dispatch => {
+  url = `${Config.API_URL}/reset`;
+
+  Axios.post(url)
+    .then(res => res.data)
+    .then(data => {
+      dispatch({
+        type: feeder.RESET_FEEDER,
+        payload: data
+      });
+    })
+    .catch(error => {
+      dispatch({
+        type: feeder.FAILED_TO_RESET_FEEDERR,
         error: error.message
       });
     });
