cee0ed206b1e1fff552d2e69dd90c4ea206af0fe
taskrik <tasos.krikonis@gmail.com>
2019-04-26 16:03:05 +0200

    Adds the dropdown comp to the home page

--- src/components/screens/HomePage.js
+++ src/components/screens/HomePage.js
@@ -1,20 +1,29 @@
 import React, { Component } from "react";
 import { View } from "react-native";
 import { connect } from "react-redux";
-
+import DropDown from "../shared/DropDown";
 import ToolIcon from "../shared/ToolIcon";
 
 export class HomePage extends Component {
   render() {
     return (
-      <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-        <ToolIcon
-          onPress={() => this.props.navigation.navigate("CatFeeder")}
-          label="Cat Feeder"
-          iconSize={60}
-          iconColor="white"
-          iconName="cat"
-        />
+      <View
+        style={{
+          flex: 1,
+          justifyContent: "space-around",
+          alignItems: "center"
+        }}
+      >
+        <DropDown />
+        <View>
+          <ToolIcon
+            onPress={() => this.props.navigation.navigate("CatFeeder")}
+            label="Cat Feeder"
+            iconSize={60}
+            iconColor="white"
+            iconName="cat"
+          />
+        </View>
       </View>
     );
   }
