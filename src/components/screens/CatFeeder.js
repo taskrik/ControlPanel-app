fbb05b455c0bc4330d65e438cdbea9b8a8db36f8
taskrik <tasos.krikonis@gmail.com>
2019-05-04 23:17:41 +0200

    Removes the buttons for capture and get captured snapshot

--- src/components/screens/CatFeeder.js
+++ src/components/screens/CatFeeder.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
-import { Text, View, StyleSheet } from "react-native";
+import { Text, View, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Button } from "native-base";
-import { TouchableOpacity } from "react-native-gesture-handler";
 import { connect } from "react-redux";
 
 import {
@@ -27,34 +26,53 @@ export class CatFeeder extends Component {
     const { serverMessage } = this.props.feeder;
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
+        {serverMessage && serverMessage.images && (
+          <View style={{ width: "100%", height: 300 }}>
+            <Image
+              style={{ width: 300, height: 300 }}
+              source={{
+                uri: `http://192.168.178:3000/` + `${serverMessage.images[0]}`
+              }}
+            />
+          </View>
+        )}
         <View style={styles.messageContainer}>
           {!serverMessage && (
             <Text style={{ fontSize: 18 }}>Its feeding time!</Text>
           )}
           {serverMessage && (
             <View style={{ alignItems: "center", justifyContent: "center" }}>
-              <Text style={{ fontSize: 26, marginBottom: 20 }}>
-                You fed the cat {serverMessage.feederInfo.timesUsed} times
-              </Text>
-              <View
-                style={{
-                  flexDirection: "row"
-                }}
-              >
-                <Text
-                  style={{ fontSize: 20, marginBottom: 20, marginRight: 10 }}
-                >
-                  Last: {serverMessage.feederInfo.hours[0]}
-                </Text>
-
-                <TouchableOpacity onPress={() => this.props.resetFeeder()}>
-                  <Text
-                    style={{ fontSize: 20, marginBottom: 20, color: "red" }}
-                  >
-                    Reset
+              {!!serverMessage.feederInfo && (
+                <View style={{ alignItems: "center" }}>
+                  <Text style={{ fontSize: 26, marginBottom: 20 }}>
+                    You fed the cat {serverMessage.feederInfo.timesUsed} times
                   </Text>
-                </TouchableOpacity>
-              </View>
+                  <View
+                    style={{
+                      flexDirection: "row"
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 20,
+                        marginBottom: 20,
+                        marginRight: 10
+                      }}
+                    >
+                      Last: {serverMessage.feederInfo.hours[0]}
+                    </Text>
+
+                    <TouchableOpacity onPress={() => this.props.resetFeeder()}>
+                      <Text
+                        style={{ fontSize: 20, marginBottom: 20, color: "red" }}
+                      >
+                        Reset
+                      </Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              )}
+
               <Text
                 style={{
                   fontSize: 20,
@@ -99,28 +117,6 @@ export class CatFeeder extends Component {
             }}
           >
             <Text style={styles.text}>Stop Feeder</Text>
-          </Button>
-          <Button
-            style={{
-              ...styles.button,
-              backgroundColor: "blue"
-            }}
-            onPress={() => {
-              this.props.takeSnapShot();
-            }}
-          >
-            <Text style={styles.text}>Take Snapshot</Text>
-          </Button>
-          <Button
-            style={{
-              ...styles.button,
-              backgroundColor: "blue"
-            }}
-            onPress={() => {
-              this.props.getSnapShot();
-            }}
-          >
-            <Text style={styles.text}>Get Snapshot</Text>
           </Button>
         </View>
       </View>
