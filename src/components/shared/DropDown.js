7a2a3dd9e46d9ccf16a9641f1d0663260992c2da
taskrik <tasos.krikonis@gmail.com>
2019-05-18 17:38:45 +0200

    Change the env

--- src/components/shared/DropDown.js
+++ src/components/shared/DropDown.js
@@ -1,23 +1,40 @@
 import React, { Component } from "react";
-import { View, Picker } from "react-native";
+import { Picker } from "react-native";
+import { connect } from "react-redux";
+import { changeEnv } from "../../actions/changeEnv";
 
-export default class DropDown extends Component {
+export class DropDown extends Component {
   state = {
-    env: "Pick env"
+    env: "HOME"
   };
 
+  componentDidMount() {
+    this.props.changeEnv(this.state.env);
+  }
+
   render() {
+    console.log("state", this.state.env);
     return (
       <Picker
         selectedValue={this.state.env}
-        onValueChange={(itemValue, itemIndex) =>
-          this.setState({ env: itemValue })
-        }
+        onValueChange={(itemValue, itemIndex) => {
+          this.setState({ env: itemValue });
+          this.props.changeEnv(itemValue);
+        }}
         style={{ height: 50, width: 100, marginBottom: 50 }}
       >
-        <Picker.Item label="Home" value="home" />
-        <Picker.Item label="Outside" value="Outside" />
+        <Picker.Item label="Home" value="HOME" />
+        <Picker.Item label="Out" value="OUT" />
       </Picker>
     );
   }
 }
+
+const mapStateToProps = state => ({});
+
+const mapDispatchToProps = { changeEnv };
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(DropDown);
